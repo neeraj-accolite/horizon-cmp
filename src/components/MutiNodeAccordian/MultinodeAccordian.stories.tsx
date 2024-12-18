668b87f92e75a7253ec1b3742d9e43915507d906
siddharth <siddharth.verma@bounteous.com>
2024-12-18 19:34:22 +0530

    Eslint Fixes

--- src/components/MutiNodeAccordian/MultinodeAccordian.stories.tsx
+++ src/components/MutiNodeAccordian/MultinodeAccordian.stories.tsx
@@ -4,31 +4,48 @@ import { MultiNodeAccordion } from './MultiNodeAccordian';
 import { MultiNodeAccordionProps } from './MultiNodeAccordian.model';
 
 export default {
-    title: 'Components/MultiNodeAccordion',
-    component: MultiNodeAccordion,
+  title: 'Components/MultiNodeAccordion',
+  component: MultiNodeAccordion,
 } as Meta;
 
-const Template: StoryFn<MultiNodeAccordionProps> = (args) => <MultiNodeAccordion {...args} />;
+const Template: StoryFn<MultiNodeAccordionProps> = (args) => (
+  <MultiNodeAccordion {...args} />
+);
 
 export const Default = Template.bind({});
 Default.args = {
-    items: [
-        {
-            title: 'Accordion Item 1',
-            description: 'This is the description for accordion item 1.',
-            children: <ul><li>Child Item 1</li><li>Child Item 2</li></ul>,
-        },
-        {
-            title: 'Accordion Item 2',
-            description: 'This is the description for accordion item 2.',
-            children: <ul><li>Child Item 1</li><li>Child Item 2</li></ul>,
-        },
-        {
-            title: 'Accordion Item 3',
-            description: 'This is the description for accordion item 3.',
-            children: <ul><li>Child Item 1</li><li>Child Item 2</li></ul>,
-        },
-    ],
-    height: 'auto',
-    width: '100%',
+  items: [
+    {
+      title: 'Accordion Item 1',
+      description: 'This is the description for accordion item 1.',
+      children: (
+        <ul>
+          <li>Child Item 1</li>
+          <li>Child Item 2</li>
+        </ul>
+      ),
+    },
+    {
+      title: 'Accordion Item 2',
+      description: 'This is the description for accordion item 2.',
+      children: (
+        <ul>
+          <li>Child Item 1</li>
+          <li>Child Item 2</li>
+        </ul>
+      ),
+    },
+    {
+      title: 'Accordion Item 3',
+      description: 'This is the description for accordion item 3.',
+      children: (
+        <ul>
+          <li>Child Item 1</li>
+          <li>Child Item 2</li>
+        </ul>
+      ),
+    },
+  ],
+  height: 'auto',
+  width: '100%',
 };
