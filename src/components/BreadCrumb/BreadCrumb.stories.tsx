b6e86fbff5408b71fe731a0513950e88994c006e
amankhuranabounteous <Aman.khurana@bounteous.com>
2024-12-19 14:57:05 +0530

    BreadCrumb component

---
+++ src/components/BreadCrumb/BreadCrumb.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import BreadCrumb, { BreadCrumbProps } from './BreadCrumb'; // Correct path
+
+export default {
+  title: 'Components/BreadCrumb', // This is required
+  component: BreadCrumb, // Link the component to the stories
+} as Meta;
+
+const Template: StoryFn<BreadCrumbProps> = (args) => <BreadCrumb {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  items: [
+    { label: 'Home', href: '/' },
+    { label: 'Plumbing', href: '/plumbing' },
+  ],
+};
+
+export const WithoutLinks = Template.bind({});
+WithoutLinks.args = {
+  items: [{ label: 'Home' }, { label: 'Plumbing' }],
+};
