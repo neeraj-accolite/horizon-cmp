48c0165b79814b52fc6077f66ea98a3a0ec3cd99
Rishu Singh <rishu.singh@bounteous.com>
2024-12-24 15:37:37 +0530

    Adding exports for Link and LinkText in the index.ts file for sharedComponents

--- src/libs/shared-components/index.ts
+++ src/libs/shared-components/index.ts
@@ -2,5 +2,7 @@ import { Accordion } from './Accordion';
 import { BreadCrumb } from './BreadCrumb';
 import { Button } from './Button';
 import { Card } from './Card';
+import { Link } from './Link';
+import { LinkText } from './LinkText';
 
-export { Accordion, Button, BreadCrumb, Card };
+export { Accordion, Button, BreadCrumb, Card, Link, LinkText };
