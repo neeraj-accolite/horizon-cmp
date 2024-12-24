6761aae870c1f61dfb5a93851f389f8132be87c6
Neeraj Agrawal <neeraj.agrawal@nutrien.com>
2024-12-24 17:55:58 +0530

    removed commented code

--- src/libs/shared-components/index.ts
+++ src/libs/shared-components/index.ts
@@ -4,15 +4,6 @@ import { Button } from './Button';
 import { Card } from './Card';
 import { Link } from './Link';
 import { LinkText } from './LinkText';
-// import {
-//   CardMembership,
-//   ChevronRight,
-//   DownArrow,
-//   LocalOffer,
-//   Star,
-//   UpArrow,
-//   UserAvatar,
-// } from './Icons';
 import * as Icons from './Icons';
 import ArrowLeft from './Icons/IconList/ArrowLeft';
 
