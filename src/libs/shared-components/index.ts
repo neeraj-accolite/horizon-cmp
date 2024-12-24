0a9496857c7f006b0a70974450c4349837144414
siddharth <siddharth.verma@bounteous.com>
2024-12-24 15:53:12 +0530

    Moved Icons to shared folder

--- src/libs/shared-components/index.ts
+++ src/libs/shared-components/index.ts
@@ -2,5 +2,28 @@ import { Accordion } from './Accordion';
 import { BreadCrumb } from './BreadCrumb';
 import { Button } from './Button';
 import { Card } from './Card';
+import {
+  CardMembership,
+  ChevronRight,
+  DownArrow,
+  LocalOffer,
+  Star,
+  UpArrow,
+  UserAvatar,
+} from './Icons';
+import ArrowLeft from './Icons/IconList/ArrowLeft';
 
-export { Accordion, Button, BreadCrumb, Card };
+export {
+  Accordion,
+  Button,
+  BreadCrumb,
+  Card,
+  ArrowLeft,
+  CardMembership,
+  ChevronRight,
+  DownArrow,
+  LocalOffer,
+  Star,
+  UpArrow,
+  UserAvatar,
+};
