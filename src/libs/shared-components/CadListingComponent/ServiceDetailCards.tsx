3c8a8c62e12ef59b0947068b07d94912f5ed139d
Neeraj Agrawal <neeraj.agrawal@accolitedigital.com>
2025-01-17 00:50:39 +0530

    afs

--- src/libs/shared-components/CadListingComponent/ServiceDetailCards.tsx
+++ src/libs/shared-components/CadListingComponent/ServiceDetailCards.tsx
@@ -6,7 +6,7 @@ import CardContent from './ServiceDetailCardsContent';
 import styles from './ServiceDetailCards.module.scss';
 import PlumbingVector from '../Icons/IconList/PlumbingVector';
 
-///comments addasas
+///comments addasa
 const ServiceDetailCards: React.FC<ServiceDetailCardsProps> = ({
   title,
   cards,
